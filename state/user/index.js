fb9fc56ba5353fb80d5642376720f6ec6b60fcf2
YolandaKisses <jelare_svken@qq.com>
2022-10-12 13:34:30 +0800

    fix

--- state/user/index.js
+++ state/user/index.js
@@ -7,7 +7,7 @@ const Mock = require("mockjs");
 const nodexlsx = require("node-xlsx");
 const send = require("koa-send");
 const fs = require("fs");
-const path = require("path")
+const path = require("path");
 
 const router = new Router({
   //设置接口前缀
@@ -35,9 +35,7 @@ for (let i = 0; i < count; i++) {
 //ctx.request.body Post参数
 router.get("/list", async (ctx) => {
   // 接收get传参
-  const {
-    selectVal
-  } = ctx.query;
+  const { selectVal } = ctx.query;
   if (selectVal) {
     // 定义过滤数组
     let filterlist = [];
@@ -62,9 +60,7 @@ router.get("/list", async (ctx) => {
 //ctx.request.body Post参数
 router.post("/delete", async (ctx) => {
   // 接受post传参
-  const {
-    id
-  } = ctx.request.body;
+  const { id } = ctx.request.body;
   // 判断id存在则根据id找到目标数据调用splice方式删除
   if (id) {
     const _index = userList.findIndex((item) => item.id === id);
@@ -88,9 +84,7 @@ router.post("/delete", async (ctx) => {
 //ctx.request.body Post参数
 router.post("/update", async (ctx) => {
   // 接受post传参
-  const {
-    row
-  } = ctx.request.body;
+  const { row } = ctx.request.body;
   // 判断row.id存在则根据row.id找到目标数据调用splice方式替换
   if (row.id) {
     const _index = userList.findIndex((item) => item.id === row.id);
@@ -116,22 +110,18 @@ router.post("/update", async (ctx) => {
 //ctx.request.body Post参数
 router.get("/download", async (ctx) => {
   // excel 导出表数据
-  let downloadList = []
+  let downloadList = [];
   // excel 表头数据
-  const data = [
-    ['id', "名称", "性别", "邮箱", "地址"]
-  ];
+  const data = [["id", "名称", "性别", "邮箱", "地址"]];
   // 接受参数
-  const {
-    selectVal
-  } = ctx.query;
+  const { selectVal } = ctx.query;
   // 如果有查询参数根据条件过滤数据后生成xlsx
   if (selectVal) {
     // 根据查询条件过滤相应数据返回
     downloadList = userList.filter((item) => item.isMale.toString() === selectVal);
   } else {
     // 如果没有查询参数直接生成xlsx
-    downloadList = JSON.parse(JSON.stringify(userList))
+    downloadList = JSON.parse(JSON.stringify(userList));
   }
   // 将list数据根据行转换为二维数组
   downloadList.forEach((item, i) => {
@@ -141,10 +131,12 @@ router.get("/download", async (ctx) => {
     }
   });
   // 生成xlsx
-  var _buffer = nodexlsx.build([{
-    name: "sheet1",
-    data: data
-  }]);
+  var _buffer = nodexlsx.build([
+    {
+      name: "sheet1",
+      data: data
+    }
+  ]);
   // 调用writeFileSync 生成download.xlsx，_buffer为文件内容
   fs.writeFileSync("download" + ".xlsx", _buffer);
   // xlsx文件位置
@@ -156,17 +148,20 @@ router.get("/download", async (ctx) => {
 
 // 上传接口 uploadfile
 // ctx.request.files.file; 获取上传文件
-router.post('/uploadfile', async (ctx, next) => {
+router.post("/uploadfile", async (ctx, next) => {
   // 上传单个文件
   const file = ctx.request.files.file; // 获取上传文件
   // 创建可读流
   const reader = fs.createReadStream(file.filepath);
-  let filePath = path.join(__dirname, '../upload') + `/${file.originalFilename}`;
+  let filePath = path.join(__dirname, "../upload") + `/${file.originalFilename}`;
   // 创建可写流
   const upStream = fs.createWriteStream(filePath);
   // 可读流通过管道写入可写流
   reader.pipe(upStream);
-  return ctx.body = "上传成功！";
+  ctx.body = {
+    data: "上传成功",
+    resutCode: "1"
+  };
 });
 
 // 导出 router 实例
